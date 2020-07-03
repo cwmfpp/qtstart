163bae4513722b46d6220b6a0b02379b894889db
cwm <wenminchen@126.com>
2020-07-03 14:05:15 +0800

    rtspclient写文件

--- demo/rtspclientdemo/src/rtspclientdemo.cpp
+++ demo/rtspclientdemo/src/rtspclientdemo.cpp
@@ -1,6 +1,44 @@
+#include <stdio.h>
+
 #include "rtspclientdemo.h"
 #include "rtspclient_self.h"
 
+static int WriteRTSPClientFile(int _iType, u_int8_t *_pucData, int _iDataLen)
+{
+    static FILE *phRTSP = NULL;
+    int iRet = 0;
+
+    UsageEnvironment* env = RTSPClientSession::m_penv;
+
+    if(RTSPC_CALLBACK_TYPE_MEDIA_DATA == _iType) {
+        if (NULL == _pucData || _iDataLen <= 0) {
+            return -1;
+        }
+
+        if(NULL == phRTSP) {
+            phRTSP = fopen("rtsp.h264", "a+");
+            if (NULL == phRTSP) {
+                return -1;
+            }
+        }
+
+        iRet = fwrite(_pucData, 1, (size_t)_iDataLen, phRTSP);
+        *env << "chenwenmin  " << __func__ << ":" <<__LINE__ << " iRet=" << iRet << "\n";
+        if (iRet != _iDataLen) {
+            *env << "chenwenmin  " << __func__ << ":" <<__LINE__ << "write failed" << "\n";
+            return -1;
+        }
+    } else if(RTSPC_CALLBACK_TYPE_SESSION_CLOSE == _iType) {
+        if (NULL != phRTSP) {
+            *env << "chenwenmin  " << __func__ << ":" <<__LINE__ << " fclose " << "\n";
+            fclose(phRTSP);
+            phRTSP = NULL;
+        }
+    }
+
+
+    return 0;
+}
 static int TestRTSPClient_CallBack(int _iType, RTSPClientAttr *_pstRTSPClientAttr, u_int8_t *_pucData, void *_pvPri)
 {
     UsageEnvironment* env = RTSPClientSession::m_penv;
@@ -12,6 +50,8 @@ static int TestRTSPClient_CallBack(int _iType, RTSPClientAttr *_pstRTSPClientAtt
         *env << "chenwenmin  " << __func__ << ":" <<__LINE__ << " _iType:" << _iType << " pcData addr:" << _pucData << " _pvPri addr:" << _pvPri << "\n";
 
     }
+
+    WriteRTSPClientFile(_iType, _pucData, (NULL != _pstRTSPClientAttr) ? _pstRTSPClientAttr->m_uiDataLen : 0);
 
     return 0;
 }
